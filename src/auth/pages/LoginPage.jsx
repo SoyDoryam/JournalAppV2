2193b5650ba3aa603606d57c957cb5d0b39afebe
SoyDoryam <shinagamialastor@gmail.com>
2025-02-07 22:00:18 -0600

    LoginPage - Diseño - Segunda parte

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const LoginPage = () => {
@@ -15,7 +17,7 @@ const LoginPage = () => {
         padding: 4
       }}
     >
-      <Grid item
+      <Grid item border={2}
         className='box-shadow'  
         xs={3}
         sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
@@ -42,7 +44,26 @@ const LoginPage = () => {
 
             </Grid>
 
+            <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
 
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  <Google /> 
+                  <Typography sx={{ml: 1}}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+          </Grid>
+          <Grid container direction='row' justifyContent='end'>
+            <Link component={ RouterLink } color='inherit' to='/auth/register'>
+              Crear una cuenta
+            </Link>
           </Grid>
         </form>
 
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,13 @@
+import { Link, Typography } from '@mui/material'
 import React from 'react'
+import { Link as RouterLink} from 'react-router-dom'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <>
+      <Typography variant='h3'>RegisterPage</Typography>
+      <Link component={ RouterLink } to='/auth/Login'>regresar</Link>
+    </>
   )
 }
 
