0a04952234e8cb766f295e06f916e1be377c05a0
SoyDoryam <shinagamialastor@gmail.com>
2025-02-07 21:14:03 -0600

    LoginPage - Diseño sin Layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,54 @@
+import { Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid 
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'primary.main',
+        padding: 4
+      }}
+    >
+      <Grid item
+        className='box-shadow'  
+        xs={3}
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
+
+        <form>
+          <Grid container>
+            <Grid item xs={12} >
+              <TextField 
+              label='Correo' 
+              type='email'
+              placeholder='correo@google.com'
+              fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField 
+              label='Contaseña' 
+              type='password'
+              placeholder='Contraceña'
+              fullWidth
+              />
+
+            </Grid>
+
+
+          </Grid>
+        </form>
+
+      </Grid>
+
+    </Grid>
   )
 }
 
