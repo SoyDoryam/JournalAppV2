cc0008743458ee8637645bb419c521f5de7bad45
SoyDoryam <shinagamialastor@gmail.com>
2025-02-09 15:56:58 -0600

    Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,30 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { IconButton } from '@mui/material'
 import JournalLayout from '../layout/JournalLayout'
 import NothingSelectedView from '../views/NothingSelectedView'
 import NoteView from '../views/NoteView'
+import { AddOutlined } from '@mui/icons-material'
 
 const JournalPage = () => {
   return (
     <JournalLayout>
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
       
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}} />
+      </IconButton>
+
     </JournalLayout>
   )
 }
